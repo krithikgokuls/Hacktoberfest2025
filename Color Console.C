036a316774467b65a22d1809b084d0c3980a0606
Krithik Gokul S <70182240+krithikgokuls@users.noreply.github.com>
2025-10-06 09:08:45 +0530

    Create Color Console.C++

---
+++ Color Console.C
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Text color: Magenta (35), Background color: Green (42)
+    cout << "\033[35;42mT4TEQ\033[0m" << endl;
+    return 0;
+}
